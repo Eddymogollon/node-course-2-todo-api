ca06681caa85f7a22f9354a22e35b97756c7e7fc
Eddy Mogollon <eddymogollon1@gmail.com>
2017-05-27 14:19:30 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var { mongoose } = require('./db/mongoose');
-var { Todo } = require('./models/todo');
-var { User } = require('./models/user');
+const { mongoose } = require('./db/mongoose');
+const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,25 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    console.log('Id is not valid');
+    return res.status(404).send();
+  } 
+
+  Todo.findById(id).then((todo) => {
+  	if (!todo) {
+		  return res.status(404).send();
+	  }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {
